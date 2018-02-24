f8466e9517e5a5b9bcf750048c7541313f3e5d18
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-02-24 20:56:36 +0100

    Adjust paper size to A4

--- make.sh
+++
@@ -1,8 +0,0 @@
-CHAPTERS=`cat chapters.list | tr '\n' ' chapters/'`
-PREAMBLE="tex/preamble.tex"
-BIBLIOGRAPHY="thesis.bib"
-CSL="csl/nature.csl"
-
-echo "pandoc'ing $CHAPTERS to thesis.pdf"
-
-pandoc --bibliography=$BIBLIOGRAPHY --csl=$CSL --metadata link-citations=true --pdf-engine=xelatex -H $PREAMBLE -V fontsize=12pt -V documentclass:book -V papersize:a4paper -V classoption:openright -V subparagraph --top-level-division=chapter $CHAPTERS -s -o thesis.pdf
---
+++ write-my-thesis.sh
@@ -0,0 +1,8 @@
+CHAPTERS=`cat chapters.list | tr '\n' ' chapters/'`
+PREAMBLE="tex/preamble.tex"
+BIBLIOGRAPHY="thesis.bib"
+CSL="csl/nature.csl"
+
+echo "pandoc'ing $CHAPTERS to thesis.pdf"
+
+pandoc --bibliography=$BIBLIOGRAPHY --csl=$CSL --metadata link-citations=true --pdf-engine=xelatex -H $PREAMBLE -V fontsize=12pt -V documentclass:book -V papersize:a4 -V classoption:openright -V subparagraph --top-level-division=chapter $CHAPTERS -s -o thesis.pdf
