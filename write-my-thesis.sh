121bd95eac84afccb8bda1d0d914eb35f2c1709c
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-02-27 21:49:07 +0100

    Add optional output format option

--- write-my-thesis.sh
+++ write-my-thesis.sh
@@ -2,7 +2,8 @@ CHAPTERS=`cat chapters.list | tr '\n' ' chapters/'`
 PREAMBLE="tex/preamble.tex"
 BIBLIOGRAPHY="thesis.bib"
 CSL="csl/pnas.csl"
+OUTFORMAT=${1:-"pdf"}
 
 echo "pandoc'ing $CHAPTERS to thesis.pdf"
 
-pandoc --bibliography=$BIBLIOGRAPHY --csl=$CSL --metadata link-citations=true --pdf-engine=xelatex -H $PREAMBLE -V fontsize=12pt -V documentclass:book -V papersize:a4 -V classoption:openright -V subparagraph --top-level-division=chapter $CHAPTERS -s -o thesis.pdf
+pandoc --bibliography=$BIBLIOGRAPHY --csl=$CSL --metadata link-citations=true --pdf-engine=xelatex -H $PREAMBLE -V fontsize=12pt -V documentclass:book -V papersize:a4 -V classoption:openright -V subparagraph --top-level-division=chapter $CHAPTERS -s -o thesis.$OUTFORMAT
