55e7a450784a64769aee2fa9655f85d28d94958e
Ulrik Günther <ulrik@42degreesoffreedom.com>
2019-02-17 20:12:03 +0100

    latexmk-fast: Use batchmode interaction for first lualatex run

--- latexmk-fast.sh
+++ latexmk-fast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-latexmk -pdf -pdflatex='lualatex -draftmode %O %S && touch %D' -print=pdf -e '$lpr_pdf=q|lualatex -interaction=batchmode -synctex=1 %R|' "$@"
+latexmk -pdf -pdflatex='lualatex -interaction=batchmode -draftmode %O %S && touch %D' -print=pdf -e '$lpr_pdf=q|lualatex -interaction=batchmode -synctex=1 %R|' "$@"
