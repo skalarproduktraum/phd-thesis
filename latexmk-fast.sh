9a5905f978fafbcc8507bb818aec15855730eca0
Ulrik Günther <ulrik@42degreesoffreedom.com>
2019-04-07 18:59:45 +0200

    latexmk-fast: Add debug option, activate by setting DEBUG=1

--- latexmk-fast.sh
+++ latexmk-fast.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-latexmk -pdf -pdflatex='lualatex -interaction=batchmode -draftmode %O %S && touch %D' -print=pdf -e '$lpr_pdf=q|lualatex -interaction=batchmode -synctex=1 %R|' "$@"
+# check if we are in debug mode, otherwise use batch mode
+if [[ -z "${DEBUG}" ]]; then
+  INTERACTION_MODE="-interaction=batchmode"
+else
+  INTERACTION_MODE=""
+fi
+
+echo "interaction mode is $INTERACTION_MODE"
+
+# run latexmk with lualatex
+latexmk -pdf -pdflatex='lualatex '"$INTERACTION_MODE"' -draftmode %O %S && touch %D' -print=pdf -e '$lpr_pdf=q|lualatex '"$INTERACTION_MODE"' -synctex=1 %R|' "$@"
+
